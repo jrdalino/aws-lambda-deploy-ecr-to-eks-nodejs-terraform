1274c6d923e97b8c44df9c456e7dc59dee240563
Jose Dalino <3316034+jrdalino@users.noreply.github.com>
2020-05-07 14:06:07 +0800

    Update state_config.tf

--- state_config.tf
+++ state_config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    region         = "ap-southeast-2"
-    bucket         = "222337787619-ap-southeast-2-terraform-state"
+    region         = "ap-southeast-1"
+    bucket         = "707538076348-ap-southeast-1-terraform-state"
     key            = "lambda.tfstate"
     encrypt        = "true"
     dynamodb_table = "terraform-state-lock"
